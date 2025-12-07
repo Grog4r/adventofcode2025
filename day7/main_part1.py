4979cfa0795c5caa48693266aef3bfae35fe8996
Niklas Küchen <niklas.kuechen@inovex.de>
2025-12-07 10:17:42 +0100

    day 7 part 1 done

---
+++ day7/main_part1.py
@@ -0,0 +1,86 @@
+class TachyonMap:
+
+    START = "S"
+    EMPTY = "."
+    BEAM = "|"
+    SPLITTER = "^"
+
+    tile_map = []
+    tile_map_simulated = []
+    used_splitter = 0
+
+    def __init__(self, file_path: str) -> None:
+        self._read_input(file_path)
+
+    def _read_input(self, file_path: str) -> None:
+        with open(file_path, "r") as f:
+            for line in f.readlines():
+                self.tile_map.append(list(line.strip()))
+
+    def print_input_map(self) -> None:
+        output_str = ""
+        for line in self.tile_map:
+            output_str += "".join(line) + "\n"
+        print(output_str)
+    
+    def print_simulated_map(self) -> None:
+        output_str = ""
+        for line in self.tile_map_simulated:
+            output_str += "".join(line) + "\n"
+        print(output_str)
+
+    def _propagate_beams_in_line(
+        self, line: list[str], beam_indices: set[int]
+    ) -> list[str]:
+        current_line = line.copy()
+        for i, symbol in enumerate(line):
+
+            if symbol == self.START:
+                beam_indices.add(i)
+                continue
+
+            elif symbol == self.SPLITTER:
+                if i in beam_indices:
+                    self.used_splitter += 1
+                    beam_indices.remove(i)
+                    prev = i - 1
+                    next = i + 1
+                    if prev >= 0:
+                        beam_indices.add(prev)
+                        current_line[prev] = self.BEAM
+                    if next < len(current_line):
+                        beam_indices.add(next)
+                        current_line[next] = self.BEAM
+                continue
+            
+            if i in beam_indices:
+                current_line[i] = self.BEAM
+
+        return current_line, beam_indices
+
+    def simulate_beams(self, VERBOSE: bool = False) -> None:
+        beam_indices = set()
+        for line in self.tile_map:
+            current_line, beam_indices = self._propagate_beams_in_line(
+                line, beam_indices
+            )
+            self.tile_map_simulated.append(current_line)
+            if VERBOSE:
+                print("".join(line))
+                print("--->")
+                print("".join(current_line))
+                print(beam_indices)
+                print()
+        print(self.used_splitter)
+
+
+def main():
+    FILE_PATH = "day7/input.txt"
+    tachyon_map = TachyonMap(FILE_PATH)
+    tachyon_map.print_input_map()
+    tachyon_map.simulate_beams()
+    # tachyon_map.print_simulated_map()
+
+
+if __name__ == "__main__":
+    main()
